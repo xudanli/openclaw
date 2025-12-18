b5b99772a3deed0f542ab1acff190e8e7229bd32
Peter Steinberger <steipete@gmail.com>
2025-12-18 23:31:34 +0100

    ClawdisKit: accept jpg for canvas.snapshot

--- apps/shared/ClawdisKit/Sources/ClawdisKit/CanvasCommandParams.swift
+++ apps/shared/ClawdisKit/Sources/ClawdisKit/CanvasCommandParams.swift
@@ -19,6 +19,24 @@ public struct ClawdisCanvasEvalParams: Codable, Sendable, Equatable {
 public enum ClawdisCanvasSnapshotFormat: String, Codable, Sendable {
     case png
     case jpeg
+
+    public init(from decoder: Decoder) throws {
+        let c = try decoder.singleValueContainer()
+        let raw = try c.decode(String.self).trimmingCharacters(in: .whitespacesAndNewlines).lowercased()
+        switch raw {
+        case "png":
+            self = .png
+        case "jpeg", "jpg":
+            self = .jpeg
+        default:
+            throw DecodingError.dataCorruptedError(in: c, debugDescription: "Invalid snapshot format: \(raw)")
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var c = encoder.singleValueContainer()
+        try c.encode(self.rawValue)
+    }
 }
 
 public struct ClawdisCanvasSnapshotParams: Codable, Sendable, Equatable {
